3cfbfcb9884e27ea9777a849bc5e69dc3c1d36d0
Jared D <81492629+jaredfdaniels@users.noreply.github.com>
2024-09-19 15:30:30 -0400

    Delete JD_09_19 directory

--- JD_09_19/src/DanielsConnectToGit.java
+++
@@ -1,9 +0,0 @@
-
-public class DanielsConnectToGit {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- JD_09_19/src/DanielsTryA.java
+++
@@ -1,9 +0,0 @@
-
-public class DanielsTryA {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
