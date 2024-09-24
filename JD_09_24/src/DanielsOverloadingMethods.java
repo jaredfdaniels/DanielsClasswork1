ced614120913d3743cf348188dffb49bc01f2720
jdaniels7 <jdaniels7@OMSB101019-P.stevenson.edu>
2024-09-24 10:01:47 -0400

    09_24 Progress

--- JD_09_24/src/DanielsOverloadingMethods.java
+++ JD_09_24/src/DanielsOverloadingMethods.java
@@ -2,14 +2,22 @@
 public class DanielsOverloadingMethods {
 
 	public static void main(String[] args) {
-		// Using multiple methods with same names (must have different returns)
+		// Using multiple methods with same names (must have different return type)
 		int num = doubleNumber(4);
 		System.out.println(num);
+		double numD = doubleNumber(4.5);
+		System.out.println(numD);
 		
 	}
 	
 	public static int doubleNumber (int n) {
-		int total = n*n;
+		int total = n * n;
 		return total;
 	}
+	
+	public static double doubleNumber (double n) {
+		double total = n * n;
+		return total;
+	}
+	
 }
