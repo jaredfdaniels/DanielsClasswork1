4143cf6f93493625f9c3bf13672ea31ae6b2b2f8
jdaniels7 <jdaniels7@OMSB101019-P.stevenson.edu>
2024-09-26 10:28:20 -0400

    Clock classes

--- JD_09_24/src/DanielsClock.java
+++ JD_09_24/src/DanielsClock.java
@@ -7,15 +7,69 @@ public class DanielsClock {
 	
 	// Constructor Method
 	public DanielsClock(){
-		hrs = 0;
-		min = 0;
-		sec = 0;
+		setTime(0,0,0);}
+	
+	public DanielsClock(int hrs, int min, int sec){
+		setTime(hrs,min,sec);}
+	
+	// Setter-Mutator Methods
+	public void setHours(int hrs){
+		if (hrs >= 0 && hrs <= 24)
+			this.hrs = hrs;
+		else this.hrs = 0;}
+	
+	public void setMinutes(int min){
+		if (min >= 0 && min <= 60)
+			this.min = min;
+		else this.min = 0;}
+	
+	public void setSeconds(int sec){
+		if (sec >= 0 && sec <= 60)
+			this.sec = sec;
+		else this.sec = 0;}
+	
+	public void incrementHours() {
+		if (this.hrs<24) this.hrs++;
+		else this.hrs=0;}
+	
+	public void incrementMinutes() {
+		if (min<60) min++;
+		else {min=0; hrs++;}}
+	
+	public void incrementSeconds() {
+		if (sec<60) sec++;
+		else {sec=0; min++;}}
+	
+	// toString Override
+	public String toString() {
+		String pClock="";
+		if(hrs<10)
+			pClock+="0";
+		pClock+=hrs+":";
+		if(min<10)
+			pClock+="0";
+		pClock+=min+":";
+		if(sec<10)
+			pClock+="0";
+		pClock+=sec;
+		return pClock;
 	}
 	
-	public DanielsClock(int call_hrs, int call_min, int call_sec){
-		hrs = call_hrs;
-		min = call_min;
-		sec = call_sec;
+	// Getter-Accessor Methods
+	public int getHours(){
+		return hrs;}
+	
+	public int getMinutes(){
+		return min;}
+	
+	public int getSeconds(){
+		return sec;}
+	
+	// Regular Methods
+	public void setTime(int hrs, int min, int sec){
+		setHours(hrs);
+		setMinutes(min);
+		setSeconds(sec);
 	}
-
+	
 }
--- JD_09_24/src/DanielsClockDriver.java
+++ JD_09_24/src/DanielsClockDriver.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 
 public class DanielsClockDriver {
 
 	public static void main(String[] args) {
 		// Same project/package --> no need to import
 		DanielsClock myClock = new DanielsClock();
-		DanielsClock yourClock = new DanielsClock(3,4,5);
+		DanielsClock yourClock = new DanielsClock(33,59,59);
+		Scanner reader = new Scanner(System.in);
+		myClock.setTime(1, 2, 3);
+		myClock.setHours(4);
+		System.out.println(yourClock.getHours());
+		System.out.println(myClock.toString());
+		myClock.incrementHours();
+		System.out.println(myClock.toString());
+		yourClock.incrementMinutes();
+		System.out.println(yourClock.toString());
+		reader.close();
+		
 	}
 
 }
